5ef53df672086fac13d6cedcec4830e108d41b83
Aarti Mishra <aarti04mishra@gmail.com>
2024-11-08 19:50:08 +0530

    Initial commit

--- config/google-oauth-config.js
+++ config/google-oauth-config.js
@@ -3,27 +3,29 @@ const { userModel } = require('../models/userModel');
 const passport=require('passport')
 
 passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret:process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL:'/auth/google/callback'
-  },
-  async function(accessToken, refreshToken, profile, cb) {
-    try{
-     let user= await userModel.findOne({email:profile.emails[0].value});
-     if(!user){
-       user=new userModel({
-        name:profile.displayName,
-        email:profile.emails[0].value
-       });
-       await user.save();
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: process.env.NODE_ENV === 'production' 
+                ? process.env.GOOGLE_CALLBACK_URL_PROD 
+                : process.env.GOOGLE_CALLBACK_URL_DEV
+},
+async function(accessToken, refreshToken, profile, cb) {
+  try {
+    let user = await userModel.findOne({ email: profile.emails[0].value });
+    if (!user) {
+      user = new userModel({
+        name: profile.displayName,
+        email: profile.emails[0].value
+      });
+      await user.save();
     }
-    cb(null,user);
-    }
-    catch(err){
-       cb(err,false);
-    }
+    cb(null, user);
+  } catch (err) {
+    cb(err, false);
   }
+}
 ));
+
 
 passport.serializeUser(function(user,cb){    //user ki id ka data session me store ho jaayegi
      return cb(null,user._id);
