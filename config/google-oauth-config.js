8aa3a3e6aa902b9e4d8365124861d4659ad18e46
Aarti Mishra <aarti04mishra@gmail.com>
2024-11-08 18:22:25 +0530

    Initial commit

--- config/google-oauth-config.js
+++ config/google-oauth-config.js
@@ -5,7 +5,7 @@ const passport=require('passport')
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret:process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL:process.env.GOOGLE_CALLBACK_URL
+    callbackURL:'/auth/google/callback'
   },
   async function(accessToken, refreshToken, profile, cb) {
     try{
